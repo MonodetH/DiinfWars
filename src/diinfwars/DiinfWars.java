08843a5ae15a8470cf27ae16d5f7a90005b2d714
MonodetH <MonodetH@MonodetH-PC.home>
2015-09-21 02:40:06 -0300

    Entrega

--- src/diinfwars/DiinfWars.java
+++ src/diinfwars/DiinfWars.java
@@ -6,7 +6,6 @@
 package diinfwars;
 
 import diinfwars.Controllers.CPrincipal;
-import diinfwars.Controllers.CLogin;
 
 import diinfwars.Models.Registro;
 import java.util.ArrayList;
@@ -18,30 +17,21 @@ import java.util.ArrayList;
 public class DiinfWars {
     
     private static CPrincipal sCPrincipal;
-    private static CLogin sCLogin;
     
     
     public static void destroyController(String controller){
         if (controller == "Principal"){sCPrincipal = null;}
-        else if(controller == "Login"){sCLogin = null;}
     }
     
     public static CPrincipal getCPrincipal(){
         if(sCPrincipal == null){sCPrincipal =new CPrincipal();}
         return sCPrincipal;
     }
-    public static CLogin getCLogin(){
-        if(sCLogin == null){sCLogin =new CLogin();}
-        return sCLogin;
-    }
-    
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        getCLogin().logearJugador(1); //Logea a la CPU
         getCPrincipal().run();
     }
     
